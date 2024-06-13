981e6aa6a1fcffc2d775f787ef2cd1959fa1e004
chinnayyachintha <chinnayyachintha>
2024-06-13 14:34:13 +0530

    all errors are solved

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ resource "aws_instance" "my-app-server" {
   ami                    = data.aws_ami.latest-amazon-linux-image.id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public_subnet_01.id
-  vpc_security_group_ids = ["aws_security_group.sg_vpc_01.id"]  
+  vpc_security_group_ids = [aws_security_group.sg_vpc_01.id]  
 
   associate_public_ip_address = true
   key_name                    = aws_key_pair.ssh-key.key_name 
